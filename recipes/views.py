d164605ca33daea872380ee554fd224f71691222
KaraokePro5070 <felipeczerniak@gmail.com>
2026-06-09 18:19:43 -0300

    Ajustes gerais

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from utils import factory
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Luiz Otávio',
+        'recipes': [factory.make_recipe() for _ in range(10)]
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Luiz Otávio',
+        'recipe': factory.make_recipe(),
+        'is_detail_page': True,
     })
